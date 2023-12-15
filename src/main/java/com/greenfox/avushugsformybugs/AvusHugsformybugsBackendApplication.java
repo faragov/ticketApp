26adad03420f93035ae0fff80cce29bdd95fe8f7
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2023-12-15 11:24:00 +0100

    style(): fix styling in java files

--- src/main/java/com/greenfox/avushugsformybugs/AvusHugsformybugsBackendApplication.java
+++ src/main/java/com/greenfox/avushugsformybugs/AvusHugsformybugsBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AvusHugsformybugsBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AvusHugsformybugsBackendApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(AvusHugsformybugsBackendApplication.class, args);
+  }
 
 }
