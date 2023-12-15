fd9b4feff4ff8be4ad98e4c3a8f2d2ed865ac7f3
mazsardonat <mazsar.donat@gmail.com>
2023-12-15 11:17:08 +0100

    checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
+    checkstyle
 }
 
 group = "com.greenfox"
@@ -22,7 +23,12 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+checkstyle {
+    toolVersion = "8.40"
+    maxWarnings = 0
+}
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
