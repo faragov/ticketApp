2c4e8d1454af16b5536f8631939e8806b817ae20
mazsardonat <mazsar.donat@gmail.com>
2024-01-11 20:11:21 +0100

    build: security dependency is added, user implements userdetails interface

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,16 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("junit:junit:4.13.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 checkstyle {
     toolVersion = "8.40"
@@ -31,5 +37,10 @@ checkstyle {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+tasks.getByName<Jar>("jar") {
+    manifest {
+        attributes["Main-Class"] = "com.kare.security.SecurityApplication"
+    }
 }
 
--- src/main/java/com/greenfox/avushugsformybugs/models/entities/User.java
+++ src/main/java/com/greenfox/avushugsformybugs/models/entities/User.java
@@ -1,12 +1,15 @@
 package com.greenfox.avushugsformybugs.models.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +31,39 @@ public class User {
   )
   @JoinColumn(name = "user_id")
   private Set<Purchase> purchases;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
 }
