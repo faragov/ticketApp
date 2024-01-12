9e960e18aa7d05623b7dadbd80c11ba40f7b13f6
mazsardonat <mazsar.donat@gmail.com>
2024-01-12 09:48:03 +0100

    build: JwtAuthenticationFilter class now exist and extends OnecPerRequestFilter

--- src/main/java/com/greenfox/avushugsformybugs/models/entities/User.java
+++ src/main/java/com/greenfox/avushugsformybugs/models/entities/User.java
@@ -2,9 +2,11 @@ package com.greenfox.avushugsformybugs.models.entities;
 
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -39,31 +41,31 @@ public class User implements UserDetails {
 
   @Override
   public String getPassword() {
-    return null;
+    return password;
   }
 
   @Override
   public String getUsername() {
-    return null;
+    return email;
   }
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isEnabled() {
-    return false;
+    return true;
   }
 }
