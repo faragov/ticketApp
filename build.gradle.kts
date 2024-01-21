ffc9d51746d252609de093f75567f6dd782a0ab4
mazsardonat <mazsar.donat@gmail.com>
2024-01-21 21:42:09 +0100

    build: validation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
+    implementation("org.hibernate.validator:hibernate-validator:6.2.0.Final")
 }
 checkstyle {
     toolVersion = "8.40"
