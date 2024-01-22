0f6a8959bec685ecf056ede5b76bfbfe70caf9e9
mazsardonat <mazsar.donat@gmail.com>
2024-01-22 16:41:10 +0100

    refractor: checkstyle

--- src/main/java/com/greenfox/avushugsformybugs/AvusHugsformybugsBackendApplication.java
+++ src/main/java/com/greenfox/avushugsformybugs/AvusHugsformybugsBackendApplication.java
@@ -1,15 +1,9 @@
 package com.greenfox.avushugsformybugs;
 
-import com.greenfox.avushugsformybugs.models.entities.User;
-import com.greenfox.avushugsformybugs.models.enums.Role;
-import com.greenfox.avushugsformybugs.repositories.UserRepository;
 import com.greenfox.avushugsformybugs.services.AdminInit;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AvusHugsformybugsBackendApplication implements CommandLineRunner {
