b6ac645f4b64e072e4ee744576f57106639155e4
mazsardonat <mazsar.donat@gmail.com>
2024-01-30 13:41:50 +0100

    feat(NewsRepository): Query for top three

--- src/main/java/com/greenfox/avushugsformybugs/repositories/NewsRepository.java
+++ src/main/java/com/greenfox/avushugsformybugs/repositories/NewsRepository.java
@@ -2,8 +2,14 @@ package com.greenfox.avushugsformybugs.repositories;
 
 import com.greenfox.avushugsformybugs.models.entities.News;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
+
+  @Query(value = "SELECT * FROM news ORDER BY number_of_clicks DESC LIMIT 3", nativeQuery = true)
+  List<News> findTopThreeNews();
 }
